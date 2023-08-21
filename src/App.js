126bc43b930bb52b9f4d6366aee4730f4a5a4a46
Abdur Rehman <abdurrehmandev6@gmail.com>
2023-08-21 21:11:11 +0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import TextSection from "./components/TextSection";
+import FormSection from "./components/FormSection";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <TextSection />
+      <FormSection />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/TextSection.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function TextSection() {
+  return (
+    <div className="text-container text-white text-center">
+      <h2 className="heading">
+        Learn to code by
+        <br />
+        watching others
+      </h2>
+      <p className="paragraph">
+        See how experienced developers solve problems in real-time. Watching
+        scripted tutorials is great, but understanding how developers think is
+        invaluable.
+      </p>
+    </div>
+  );
+}
+
+export default TextSection;
